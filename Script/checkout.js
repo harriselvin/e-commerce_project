4483deb4ad8f7e1085e011143a6260b8de7b1e86
harriselvin <harriselvin6@gmail.com>
2024-06-13 13:25:25 +0200

    made my cart fully functional

--- Script/checkout.js
+++ Script/checkout.js
@@ -9,18 +9,28 @@ let purchasedItems = JSON.parse(localStorage.getItem('purchasedItems')) || []
 // Function to render cart items
 function renderCartItem() {
     tbody.innerHTML = ''
-    purchasedItems.forEach(product => {
-        tbody.innerHTML += `
-            <tr class="tbody-row">
-                <td class="table-item tbody-img"><img src="${product.image}" alt=""></td>
-                <td class="table-item tbody-name name">${product.name}</td>
-                <td class="table-item tbody-cat">${product.category}</td>
-                <td class="table-item tbody-quantity"><span class="decrement" data-id=${product.id}>-</span> ${product.quantity} <span class="increment" data-id=${product.id}>+</span></td>
-                <td class="table-item remove-item remove">${product.price}</td>
-                <td class="table-item remove-item remove-item">Remove</td>
-            </tr>
+    if (Array.isArray(purchasedItems)) {
+        purchasedItems.forEach(product => {
+            tbody.innerHTML += `
+                <tr class="tbody-row">
+                    <td class="table-item tbody-img"><img src="${product.image}" alt=""></td>
+                    <td class="table-item tbody-name name">${product.name}</td>
+                    <td class="table-item tbody-cat">${product.category}</td>
+                    <td class="table-item tbody-quantity"><span class="decrement" data-id=${product.id}>-</span> ${product.quantity} <span class="increment" data-id=${product.id}>+</span></td>
+                    <td class="table-item">${product.price}</td>
+                    <td class="table-item remove-item remove-item" data-id=${product.id}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                    <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
+                    </svg></td>
+                </tr>
+            `
+        });
+    } if (tbody.innerHTML == '') {
+        tbody.innerHTML = `
+        <tr>
+            <td colspan="6" style="text-align: center">No Items have been added<td/>
+        </tr>
         `
-    });
+    }
 
     // Attach event listeners for increment and decrement buttons
     incrementDecrementListeners()
@@ -33,27 +43,21 @@ function totalPriceCalc() {
     let total = 0
     purchasedItems.forEach(item => {
         total += item.quantity * parseFloat(item.price)
-        amount.textContent = total.toLocaleString('en-US')
+        price.textContent = total
     })
 }
 
+// Function to calculate the total number of items in cart
 function quantityCalc() {
-    let amount = 0
+    let quanAmount = 0
     purchasedItems.forEach(item => {
-        amount = item.quantity += amount
-        amount.textContent = amount
+        quanAmount = item.quantity + quanAmount
     })
-}
-
-// Function to remove all items from cart
-function removeAllItems() {
-    purchasedItems = []
-    localStorage.setItem('purchasedItems', JSON.stringify(purchasedItems))
-    renderCartItem()
+    amount.textContent = quanAmount
 }
 
 // Function increments the quantity of an item
-function incrementItem() {
+function incrementItem(id) {
     let item = purchasedItems.find(product => product.id === +id)
     if (item) {
         item.quantity += 1
@@ -63,7 +67,7 @@ function incrementItem() {
 }
 
 // Function to decrement the quantity of an item
-function decrementItem() {
+function decrementItem(id) {
     let item = purchasedItems.find(product => product.id === +id)
     if (item && item.quantity > 1) {
         item.quantity -= 1
@@ -77,6 +81,14 @@ function removeItem(id) {
     purchasedItems = purchasedItems.filter(product => product.id !== +id)
     localStorage.setItem('purchasedItems', JSON.stringify(purchasedItems))
     renderCartItem()
+}
+
+// Function to remove all items from cart
+function removeAllItems() {
+    purchasedItems = []
+    localStorage.setItem('purchasedItems', JSON.stringify(purchasedItems))
+    renderCartItem()
+    amount.innerText
 }
 
 // Attach event listeners for increment and decrement buttons 
